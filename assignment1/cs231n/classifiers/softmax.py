7d4c1f9360ce3bafe2ae385b624dfcf4bf07b9fe
yuquan <yuquan.wang@weblab.t.u-tokyo.ac.jp>
2024-12-13 14:51:32 +0900

    Add files via upload

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -34,7 +34,26 @@ def softmax_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
+    for i in range(num_train):
+        scores = X[i].dot(W) # (1, C)
+        scores -= np.max(scores)
+        # If the maximum value is not subtracted, the calculation of exp may result in very large values, e.g. score=1000.
+        # After subtracting max the largest fraction is 0, the rest are negative and the range of exp becomes [0,1], thus avoiding overflow.
+        # When all scores are small and approaching negative infinity, the value of exp will be close to 0. 
+        # After subtracting max, the range of scores is compressed to a larger value between negative and 0 to avoid loss of precision.
+        softmax_P = np.exp(scores)/np.sum(np.exp(scores))
+        loss -= np.log(softmax_P[y[i]])
+        for j in range(num_classes):
+            dW[:,j] += X[i] * softmax_P[j]
+        dW[:,y[i]] -= X[i]
+
+    loss /= num_train
+    loss += reg * np.sum(W * W)
+    dW /= num_train
+    dW += 2 * reg * W
+
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -59,7 +78,20 @@ def softmax_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
+    scores = X.dot(W) # (N, C)
+    scores -= np.max(scores, axis=1, keepdims=True)
+    softmax_P = np.exp(scores)/np.sum(np.exp(scores), axis=1, keepdims=True)
+
+    loss = np.sum(-np.log(softmax_P[range(num_train),y]))
+    softmax_P[range(num_train),y] -= 1
+    dW = X.T @ (softmax_P)
+
+    loss /= num_train
+    loss += reg * np.sum(W * W)
+    dW /= num_train
+    dW += reg * 2 * W
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
