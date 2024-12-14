2d6b4cd306772c05ec4c40a14834a74f29b4b2a7
yuquan <yuquan.wang@weblab.t.u-tokyo.ac.jp>
2024-12-14 17:16:11 +0900

    Add files via upload

--- assignment1/cs231n/classifiers/fc_net.py
+++ assignment1/cs231n/classifiers/fc_net.py
@@ -1,5 +1,6 @@
 from builtins import range
 from builtins import object
+from re import L
 import numpy as np
 
 from ..layers import *
@@ -55,7 +56,11 @@ class TwoLayerNet(object):
         ############################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        self.params['W1'] = np.random.normal(loc=0.0, scale=weight_scale, size=(input_dim,hidden_dim))
+        self.params['b1'] = np.zeros(hidden_dim)
+
+        self.params['W2'] = np.random.normal(loc=0.0, scale=weight_scale, size=(hidden_dim,num_classes))
+        self.params['b2'] = np.zeros(num_classes)
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         ############################################################################
@@ -88,7 +93,8 @@ class TwoLayerNet(object):
         ############################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        out1, cache1 = affine_relu_forward(X, self.params['W1'], self.params['b1'])
+        scores, cache2 = affine_forward(out1, self.params['W2'], self.params['b2'])
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         ############################################################################
@@ -112,7 +118,15 @@ class TwoLayerNet(object):
         ############################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        loss, dscores = softmax_loss(scores, y)
+        loss += 0.5 * self.reg * np.sum(self.params['W1']**2)
+        loss += 0.5 * self.reg * np.sum(self.params['W2']**2)
+
+        dout, grads['W2'], grads['b2'] = affine_backward(dscores,cache2)
+        dout1, grads['W1'], grads['b1'] = affine_relu_backward(dout,cache1)
+
+        grads['W2'] += self.reg * self.params['W2']
+        grads['W1'] += self.reg * self.params['W1']
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         ############################################################################
